7c64b91e97d257338096e5631ed5d870947872ff
Karla038 <karladk038@gmail.com>
2025-04-10 19:36:34 -0600

    Inicio del proyecto

---
+++ src/main/java/com/taskly/TasklyBApplication.java
@@ -0,0 +1,13 @@
+package com.taskly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklyBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklyBApplication.class, args);
+	}
+
+}
